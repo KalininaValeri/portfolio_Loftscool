a2b8759a6f4c0bd93b36609ae406821e4c8b4693
KalininaValeri <kalinina.valeri@gmail.com>
2017-02-12 23:51:12 +0700

    restructuring/output ready files

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,40 @@
 'use strict';
 
 global.$ = {
-  package: require('./package.json'),
-  config: require('./gulp/config'),
-  path: {
-    task: require('./gulp/paths/tasks.js'),
-    jsFoundation: require('./gulp/paths/js.foundation.js'),
-    cssFoundation: require('./gulp/paths/css.foundation.js'),
-    app: require('./gulp/paths/app.js')
-  },
-  gulp: require('gulp'),
-  del: require('del'),
-  browserSync: require('browser-sync').create(),
-  gp: require('gulp-load-plugins')()
+    package: require('./package.json'),
+    config: require('./gulp/config'),
+    path: {
+        task: require('./gulp/paths/tasks.js'),
+        jsFoundation: require('./gulp/paths/js.foundation.js'),
+        cssFoundation: require('./gulp/paths/css.foundation.js'),
+        app: require('./gulp/paths/app.js')
+    },
+    gulp: require('gulp'),
+    del: require('del'),
+    browserSync: require('browser-sync').create(),
+    gp: require('gulp-load-plugins')()
 };
 
-$.path.task.forEach(function(taskPath) {
-  require(taskPath)();
+$.path.task.forEach(function (taskPath) {
+    require(taskPath)();
 });
 
 $.gulp.task('default', $.gulp.series(
-  'clean',
-  $.gulp.parallel(
-    'sass',
-    'pug',
-    'js:foundation',
-    'js:process',
-    'copy:image',
-    'css:foundation',
-    'sprite:svg'
-  ),
-  $.gulp.parallel(
-    'watch',
-    'serve'
-  )
+    'clean',
+    $.gulp.parallel(
+        'sass',
+        'pug',
+        'js:foundation',
+        'js:process',
+        'copy:image',
+        'css:foundation',
+        'sprite:svg',
+        'sprite'
+    ),
+    $.gulp.parallel(
+        'watch',
+        'serve'
+    )
 ));
 
-$.gulp.task('sprite', function () {
-  return $.gulp.src('./source/images/sprites/png/*.*') // путь, откуда берем картинки для спрайта
-      .pipe($.gp.spritesmith({
-        imgName: 'sprite.png',
-        cssName: 'sprite.css'
-      }))
-      .pipe($.gp.if('*.png', $.gulp.dest('./build/assets/img/')))
-      .pipe($.gp.if('*.css', $.gulp.dest('./build/assets/css/')));
-});
+
