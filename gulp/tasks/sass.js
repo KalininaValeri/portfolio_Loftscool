ead2f60b236c39fe01fd5d6f14cc311fd038634d
KalininaValeri <kalinina.valeri@gmail.com>
2017-02-22 20:15:06 +0700

    css-unit

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -4,6 +4,10 @@ module.exports = function () {
     $.gulp.task('sass', function () {
         return $.gulp.src('./source/style/app.scss')
             .pipe($.gp.sourcemaps.init())
+            .pipe($.gp.cssUnit({
+                type     :    'px-to-rem',
+                rootSize :    16
+            }))
             .pipe($.gp.sassGlob())
             .pipe($.gp.sass()).on('error', $.gp.notify.onError({title: 'Style'}))
             .pipe($.gp.autoprefixer({browsers: $.config.autoprefixerConfig}))
