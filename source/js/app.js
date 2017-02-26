a6095e36a81e2cbda3deb9d10a0e731c24dafed5
KalininaValeri <kalinina.valeri@gmail.com>
2017-02-26 16:56:21 +0700

    block map. map api. error

--- source/js/app.js
+++ source/js/app.js
@@ -5,3 +5,13 @@
     document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
 })();
+
+$(function () {
+    var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+    }
+});
