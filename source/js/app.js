1e6e997fcc5ca03437da03ec46201051f742f476
KalininaValeri <kalinina.valeri@gmail.com>
2017-02-28 15:19:36 +0300

    thinges

--- source/js/app.js
+++ source/js/app.js
@@ -6,27 +6,27 @@
     }, 1000);
 })();
 
-// var blur = (function () {
-//     var container = $('.c-form-container'),
-//         form = $('.c-form-wrapper');
-//
-//     return {
-//         set: function () {
-//             var img = $('.c-block-bg_pic'),
-//                 imgWidth = img.width(),
-//                 imgHeight = img.height(),
-//                 posLeft = -container.offset().left,
-//                 posTop = -container.position().top;
-//
-//             // blurCss.backgroundSize = imgWidth + 'px' + ' ' + imgHeight + 'px';
-//             // blurCss.backgroundPosition = posLeft + 'px' + ' ' + posTop + 'px';
-//             form.css({
-//                 'background-size': imgWidth + 'px' + ' ' + imgHeight + 'px',
-//                 'background-position': posLeft + 'px' + ' ' + posTop + 'px'
-//             });
-//         }
-//     }
-// }());
+var blur = (function () {
+    var container = $('.c-form-container'),
+        form = $('.c-form-wrapper');
+
+    return {
+        set: function () {
+            var img = $('.c-block-bg_pic'),
+                imgWidth = img.width(),
+                imgHeight = img.height(),
+                posLeft = -container.offset().left,
+                posTop = -container.position().top;
+
+            // blurCss.backgroundSize = imgWidth + 'px' + ' ' + imgHeight + 'px';
+            // blurCss.backgroundPosition = posLeft + 'px' + ' ' + posTop + 'px';
+            form.css({
+                'background-size': imgWidth + 'px' + ' ' + imgHeight + 'px',
+                'background-position': posLeft + 'px' + ' ' + posTop + 'px'
+            });
+        }
+    }
+}());
 
 var parallax = (function () {
     var bg = document.querySelector('.l-hero__bg');
@@ -82,13 +82,13 @@ $(function () {
         parallax.init(wScroll);
     };
 
-    // blur.set();
-    // $(window).resize(function () {
-    //     blur.set();
-    // });
-
     window.addEventListener('mousemove', function (e) {
         parallaxMouse(e);
+    });
+
+    blur.set();
+    $(window).resize(function () {
+        blur.set();
     });
 
 });
