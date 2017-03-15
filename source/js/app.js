efac42343b1b5ed9d18734ca189b16287ad4a40d
KalininaValeri <kalinina.valeri@gmail.com>
2017-03-15 23:42:20 +0700

    block header.parallax scroll debage. background night sky

--- source/js/app.js
+++ source/js/app.js
@@ -43,30 +43,6 @@ var blur = (function () {
     }
 }());
 
-var parallax = (function () {
-    var bg = document.querySelector('.l-hero__bg');
-    var user = document.querySelector('.c-user');
-    var fon = document.querySelector('.c-user__bg');
-
-    return {
-        move: function (block, windowScroll, strafeAmound) {
-            var strafe = windowScroll / -strafeAmound + '%';
-            var transformString = 'translate3d(0,' + strafe + ', 0)';
-            var style = block.style;
-
-            style.transform = transformString;
-            style.webkitTransform = transformString;
-
-        },
-        init: function (wScroll) {
-            this.move(bg, wScroll, 50);
-            this.move(fon, wScroll, 20);
-        }
-    }
-
-}());
-
-
 var App = (function () {
     return{
         init: function () {
@@ -102,6 +78,13 @@ var App = (function () {
                 console.log('paralaxMouse');
                 parallaxMouse.init();
             }
+
+            if (!!(document.querySelector('.l-hero__bg'))) {
+                window.onscroll = function () {
+                    var wScroll = window.pageYOffset;
+                    parallax.init(wScroll);
+                };
+            }
         }
     }
 })();
@@ -114,10 +97,7 @@ $(function () {
 
     App.init();
 
-    // window.onscroll = function () {
-    //     var wScroll = window.pageYOffset;
-    //     parallax.init(wScroll);
-    // };
+
 
 
 
---
+++ source/js/modules/parallax-scroll.js
@@ -0,0 +1,25 @@
+var parallax = (function () {
+    if (!(document.querySelector('.l-hero__bg'))) {
+        return false
+    }
+
+    var bg = document.querySelector('.l-hero__bg');
+    var user = document.querySelector('.c-user');
+    var fon = document.querySelector('.c-user__bg');
+
+    return {
+        move: function (block, windowScroll, strafeAmound) {
+            var strafe = windowScroll / -strafeAmound + '%';
+            var transformString = 'translate3d(0,' + strafe + ', 0)';
+            var style = block.style;
+
+            style.transform = transformString;
+            style.webkitTransform = transformString;
+
+        },
+        init: function (wScroll) {
+            this.move(bg, wScroll, 50);
+            this.move(fon, wScroll, 20);
+        }
+    }
+}());
